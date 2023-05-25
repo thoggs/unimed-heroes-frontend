86a5c30eec11de125436dd81b4b47900cc6ed533
Thiago Rodrigues <thiagothogs@icloud.com>
2023-05-25 19:46:44 -0300

    adjusted server file

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 const express = require('express');
+const serveStatic = require("serve-static")
 const path = require('path');
-
-const app = express();
-
-// Defina a pasta 'dist' como o diretório estático
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Defina uma rota para responder ao acesso à página inicial
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-// Defina a porta em que o servidor irá ouvir
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Servidor iniciado na porta ${port}`);
-});
+app.listen(port);
+
